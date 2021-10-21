d111de446462d43cbb50a1172a91d1d927d51ad0
Philipp Schweiger <phips0812@gmail.com>
2021-10-21 11:22:43 +0200

    Adapted example

--- example/client.js
+++ example/client.js
@@ -9,70 +9,79 @@ const authMetaData = {
 runVTSIClientSample(endPoint, authMetaData);
 
 function runVTSIClientSample(endPoint, authMetaData){
+	const firstName = 'Philipp';
+	const lastName = 'Schweiger';
+	const phoneNumber = '+436605555555';
+	const callId = '8804ddf0-e66e-44b5-a8c7-2891fe1d3f94';
+	const projectId = 'f1daa016-f997-4729-a5e3-0bbf0725b439';
 
-	const startCallInstanceRequest = vtsi.StartCallInstanceRequest;
-	startCallInstanceRequest.phoneNumber = '<INSERT PHONE NUMBER>';
-	startCallInstanceRequest.callId = '<GENERATE SOME ID>';
+	const startCallInstanceRequest = new vtsi.StartCallInstanceRequest();
+	startCallInstanceRequest.phoneNumber = phoneNumber;
+	startCallInstanceRequest.callId = callId;
 	startCallInstanceRequest.sipSimVersion = '1.0.0';
-	startCallInstanceRequest.projectId = 'f1daa016-f997-4729-a5e3-0bbf0725b439'; // EXAMPLE
+	startCallInstanceRequest.projectId = projectId;
 	startCallInstanceRequest.initText = '';
 	startCallInstanceRequest.sipPrefix = '';
 	startCallInstanceRequest.sipName = '';
 	startCallInstanceRequest.initialIntent = 'i.intro.hello';
 
-	const startCallContext = vtsi.Context;
-	startCallContext.name = 'projects/f1daa016-f997-4729-a5e3-0bbf0725b439/agent/sessions/8804ddf0-e66e-44b5-a8c7-2891fe1d3f91/contexts/c-parameters';
+	const startCallContext = new vtsi.Context();
+	startCallContext.name = `projects/${projectId}/agent/sessions/${callId}/contexts/c-parameters`;
 	startCallContext.lifespanCount = 1000;
 	startCallContext.lifespanTime = 600;
-	const firstName = vtsi.Context.Parameter;
-	firstName.name = '';
-	firstName.displayName = 'p.first_name';
-	firstName.value = 'Philipp';
-	firstName.valueOriginal = 'Philipp';
-	startCallContext.parametersMap.push(firstName);
-	const lastName = vtsi.Context.Parameter;
-	firstName.name = '';
-	firstName.displayName = 'p.last_name';
-	firstName.value = 'Schweiger';
-	firstName.valueOriginal = 'Schweiger';
-	startCallContext.parametersMap.push(lastName);
-	const phoneNumber = vtsi.Context.Parameter;
-	firstName.name = '';
-	firstName.displayName = 'p.phone_number';
-	firstName.value = '+436605555555';
-	firstName.valueOriginal = '+436605555555';
-	startCallContext.parametersMap.push(phoneNumber);
+	startCallContext.parameters = new Map();
+
+	const firstNameContextParameter = new vtsi.Context.Parameter();
+	firstNameContextParameter.name = '';
+	firstNameContextParameter.displayName = 'p.first_name';
+	firstNameContextParameter.value = firstName;
+	firstNameContextParameter.valueOriginal = firstName;
+	startCallContext.parameters['p.first_name'] = firstNameContextParameter;
+	const lastNameContextParameter = new vtsi.Context.Parameter();
+	lastNameContextParameter.name = '';
+	lastNameContextParameter.displayName = 'p.last_name';
+	lastNameContextParameter.value = lastName;
+	lastNameContextParameter.valueOriginal = lastName;
+	startCallContext.parameters['p.last_name'] = lastNameContextParameter;
+	const phoneNumberContextParameter = new vtsi.Context.Parameter();
+	phoneNumberContextParameter.name = '';
+	phoneNumberContextParameter.displayName = 'p.phone_number';
+	phoneNumberContextParameter.value = phoneNumber;
+	phoneNumberContextParameter.valueOriginal = phoneNumber;
+	startCallContext.parameters['p.phone_number'] = phoneNumberContextParameter;
 	startCallInstanceRequest.context = startCallContext;
 
-	const asteriskConfig = vtsi.ServiceConfig;
-	asteriskConfig.host = '<INSERT HOST>';
-	asteriskConfig.port = '<INSERT PORT>';
+	const asteriskConfig = new vtsi.ServiceConfig();
+	asteriskConfig.host = '192.168.1.111';
+	asteriskConfig.port = '5060';
 	asteriskConfig.serviceIdentifier = 'asterisk';
 	asteriskConfig.languageCode = 'en';
 	startCallInstanceRequest.asteriskConfig = asteriskConfig;
 
-	const caiConfig = vtsi.ServiceConfig;
-	asteriskConfig.host = '<INSERT HOST>';
-	asteriskConfig.port = '<INSERT PORT>';
+	const caiConfig = new vtsi.ServiceConfig();
+	asteriskConfig.host = 'grpc-nlu-develop.ondewo.com';
+	asteriskConfig.port = '443';
 	asteriskConfig.serviceIdentifier = '';
 	asteriskConfig.languageCode = 'en';
 	startCallInstanceRequest.caiConfig = caiConfig;
 
-	const sttConfig = vtsi.ServiceConfig;
-	asteriskConfig.host = '<INSERT HOST>';
-	asteriskConfig.port = '<INSERT PORT>';
+	const sttConfig = new vtsi.ServiceConfig();
+	asteriskConfig.host = 'grpc-s2t.ondewo.com';
+	asteriskConfig.port = '443';
 	asteriskConfig.serviceIdentifier = 'ONDEWO';
 	asteriskConfig.languageCode = 'default_english';
 	startCallInstanceRequest.sttConfig = sttConfig;
 
-	const ttsConfig = vtsi.ServiceConfig;
-	asteriskConfig.host = '<INSERT HOST>';
-	asteriskConfig.port = '<INSERT PORT>';
+	const ttsConfig = new vtsi.ServiceConfig();
+	asteriskConfig.host = 'grpc-t2s.ondewo.com';
+	asteriskConfig.port = '443';
 	asteriskConfig.serviceIdentifier = 'ONDEWO';
 	asteriskConfig.languageCode = 'changyee';
 	startCallInstanceRequest.ttsConfig = ttsConfig;
 
-	const vtsiClient = createVTSIClient(host);
+	console.log(startCallInstanceRequest)
+
+	const vtsiClient = createVTSIClient(endPoint);
 	vtsiClient.startCallInstance(startCallInstanceRequest, authMetaData)
 		.then(response => {
 			if (response.success) {
