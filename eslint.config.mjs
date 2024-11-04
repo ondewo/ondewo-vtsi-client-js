d9d3355ee0a7a7502f459bdfae8cc89669202f31
Andreas Rath <andreas.rath@ondewo.com>
2024-11-04 16:58:10 +0100

    Fix globals in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,212 +1,97 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js'; // Core ESLint configuration
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import {fileURLToPath} from 'node:url';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
 export default [
-	{
-		ignores: ['**/api/', 'src/ondewo-vtsi-api', '**/ondewo-proto-compiler', '**/*.mjs', 'example/client.js']
-	},
-	...compat.extends(
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking'
-	),
-	{
-		plugins: {
-			'@typescript-eslint': typescriptEslint
-		},
+    {
+        ignores: ['**/api/', 'src/ondewo-vtsi-api', '**/ondewo-proto-compiler']
+    },
+    {
+        files: ['**/*.js'], // Target all JavaScript files
+        languageOptions: {
+            globals: {
+                // Add any global variables you want to recognize
+                window: true,
+                document: true,
+                console: true,
+                XMLHttpRequest: true,
+                uuidv4: true,
+                vtsi: true,
+            },
+            ecmaVersion: 2020, // Set ECMAScript version
+            sourceType: 'module' // If using ES modules
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            'prefer-const': 'error',
+            'no-trailing-spaces': ['error'],
+            eqeqeq: ['error', 'always'],
+            semi: ['error', 'always'],
+            'id-denylist': ['error', 'err', 'any', 'cb', 'callback', 'i1', 'test', 'collection', 'list'],
 
-		languageOptions: {
-			globals: {
-				...globals.browser
-			},
+            'no-multiple-empty-lines': ['error'],
+            'no-new-wrappers': ['error'],
+            'no-var': ['error'],
+            'no-multi-spaces': 'error',
+            'block-spacing': ['error', 'always'],
 
-			parser: tsParser,
-			ecmaVersion: 12,
-			sourceType: 'module',
+            'brace-style': [
+                'error',
+                '1tbs',
+                {
+                    allowSingleLine: true
+                }
+            ],
 
-			parserOptions: {
-				project: 'tsconfig.json'
-			}
-		},
+            'comma-spacing': [
+                'error',
+                {
+                    before: false,
+                    after: true
+                }
+            ],
 
-		rules: {
-			'@typescript-eslint/explicit-member-accessibility': [
-				'error',
-				{
-					accessibility: 'explicit',
+            'semi-spacing': [
+                'error',
+                {
+                    before: false,
+                    after: true
+                }
+            ],
+            'no-ternary': 'warn',
+            'space-in-parens': ['error', 'never'],
+            'space-before-blocks': ['error', 'always'],
+            'no-return-assign': 'error',
+            'no-mixed-operators': 'warn',
+            'no-nested-ternary': 'error',
+            'no-unneeded-ternary': 'error',
+            'prefer-exponentiation-operator': 'error',
 
-					overrides: {
-						accessors: 'explicit',
-						parameterProperties: 'explicit'
-					}
-				}
-			],
+            'arrow-spacing': [
+                'error',
+                {
+                    before: true,
+                    after: true
+                }
+            ],
 
-			'@typescript-eslint/no-inferrable-types': ['off'],
+            'func-call-spacing': ['error', 'never'],
 
-			'@typescript-eslint/array-type': [
-				'error',
-				{
-					default: 'array'
-				}
-			],
-
-			'@typescript-eslint/unbound-method': [
-				'warn',
-				{
-					ignoreStatic: true
-				}
-			],
-
-			'@typescript-eslint/no-unsafe-member-access': ['warn'],
-			'@typescript-eslint/no-this-alias': ['warn'],
-
-			'@typescript-eslint/no-misused-promises': [
-				'error',
-				{
-					checksVoidReturn: false
-				}
-			],
-
-			'@typescript-eslint/no-unsafe-call': ['off'],
-			'@typescript-eslint/no-unused-vars': ['off'],
-			'no-whitespace-before-property': ['error'],
-			'no-trailing-spaces': ['error'],
-
-			'@typescript-eslint/no-shadow': [
-				'error',
-				{
-					hoist: 'all'
-				}
-			],
-
-			'@typescript-eslint/no-misused-new': 'error',
-			'@typescript-eslint/no-floating-promises': 'error',
-
-			eqeqeq: ['error', 'always'],
-			semi: ['error', 'always'],
-
-			'id-denylist': ['error', 'err', 'any', 'cb', 'callback', 'i1', 'test', 'collection', 'list'],
-
-			'no-multiple-empty-lines': ['error'],
-			'no-new-wrappers': ['error'],
-			'prefer-const': ['error'],
-			'no-var': ['error'],
-			'no-multi-spaces': 'error',
-			'block-spacing': ['error', 'always'],
-
-			'brace-style': [
-				'error',
-				'1tbs',
-				{
-					allowSingleLine: true
-				}
-			],
-
-			'comma-spacing': [
-				'error',
-				{
-					before: false,
-					after: true
-				}
-			],
-
-			'semi-spacing': [
-				'error',
-				{
-					before: false,
-					after: true
-				}
-			],
-
-			'space-in-parens': ['error', 'never'],
-			'space-before-blocks': ['error', 'always'],
-			'no-return-assign': 'error',
-			'no-mixed-operators': 'warn',
-			'no-nested-ternary': 'error',
-			'no-unneeded-ternary': 'error',
-			'prefer-exponentiation-operator': 'error',
-
-			'arrow-spacing': [
-				'error',
-				{
-					before: true,
-					after: true
-				}
-			],
-
-			'func-call-spacing': ['error', 'never'],
-
-			'key-spacing': [
-				'error',
-				{
-					afterColon: true
-				}
-			],
-
-			'@typescript-eslint/explicit-function-return-type': ['error'],
-
-			'@typescript-eslint/typedef': [
-				'error',
-				{
-					arrowParameter: true,
-					memberVariableDeclaration: true,
-					variableDeclaration: true,
-					parameter: true,
-					propertyDeclaration: true
-				}
-			],
-
-			'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-
-			'@typescript-eslint/no-confusing-void-expression': [
-				'error',
-				{
-					ignoreArrowShorthand: true
-				}
-			],
-
-			'@typescript-eslint/prefer-reduce-type-parameter': ['error'],
-			'default-param-last': 'off',
-			'@typescript-eslint/default-param-last': ['error'],
-			'no-redeclare': 'off',
-			'@typescript-eslint/no-redeclare': ['error'],
-			'@typescript-eslint/prefer-readonly-parameter-types': ['off'],
-			'guard-for-in': ['warn'],
-			'@typescript-eslint/no-unused-vars-experimental': ['off'],
-			'@typescript-eslint/no-explicit-any': ['warn'],
-			'@typescript-eslint/no-unsafe-assignment': ['warn'],
-			'@typescript-eslint/no-unsafe-return': ['error'],
-
-			'@typescript-eslint/explicit-module-boundary-types': [
-				'error',
-				{
-					allowArgumentsExplicitlyTypedAsAny: true
-				}
-			],
-
-			'no-ternary': 'warn',
-			'@typescript-eslint/member-ordering': ['off']
-		}
-	},
-	{
-		files: ['**/*.spec.ts'],
-		ignores: ['**/node_modules/**'],
-
-		rules: {
-			'@typescript-eslint/no-floating-promises': 'off'
-		}
-	}
+            'key-spacing': [
+                'error',
+                {
+                    afterColon: true
+                }
+            ]
+        }
+    }
 ];
